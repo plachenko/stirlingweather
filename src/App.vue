367d9665fc0152d364a22615d269f337072ce648
denny <plachenko@gmail.com>
2020-01-28 08:01:52 -0500

    updated functionality

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
+    <div id="widget">
+      <h1 style="font-size: 1em; display: block; position: absolute; top: -40px;">Check Weather</h1>
+      <!-- Error Handling -->
+      <transition @enter="err_enter" @leave="err_leave">
+        <div class="error" v-if="error">Error: {{error}}</div>
+      </transition>
 
-    <!-- Error Handling -->
-    <div class="error" v-if="error">Error: {{error}}</div>
+      <!-- Weather widget display -->
+      <!-- <WeatherWidget :data="weatherData" /> -->
+      <div style="height: 100%; width: 100%; position: relative;">
+        <Map style="position: absolute;" />
+      </div>
 
-    <!-- Weather widget display -->
-    <WeatherWidget :data="weatherData" />
+      <!-- Location capture form -->
+      <a href="#" id="curLoc" @click="getBrowserLocation">use current location</a>
 
-    <!-- Location capture form -->
-    <form @submit.prevent="getData('name')">
-      <input placeholder="please enter a location" type="text" v-model="location" />
-      <input type="submit" />
-    </form>
-    <hr />
-    or
-    <a href="#" @click="getBrowserLocation">use browser current location</a>
+      <form id="locinput" @submit.prevent="getData('name')">
+        <input placeholder="please enter a location" type="text" v-model="location" />
+      </form>
 
-    <!-- <Map style="position: absolute; left: 100px; top: 300px;" /> -->
-
+    </div>
   </div>
 </template>
 
@@ -26,6 +29,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import WeatherWidget from './components/WeatherWidget.vue';
 import Map from './components/Map.vue';
 import axios from 'axios';
+import gsap from 'gsap';
 
 @Component({
   components: {
@@ -39,9 +43,13 @@ export default class App extends Vue {
   private weatherData: object = {};
   private appID: string = "3271837e9218269f1e7f49308577ec1c"
   private error: string = "";
-  
-  private mounted(){
 
+  private test: boolean = false;
+
+  private mounted(){
+    setTimeout(() => {
+      this.test = true;
+    }, 1000)
   }
 
   private getBrowserLocation(){
@@ -51,6 +59,20 @@ export default class App extends Vue {
     })
   }
 
+  private err_enter(el: any){
+    gsap.from(el, {height: 0, padding: 0, opacity: 0, onComplete:()=>{
+      el.innerHTML = `Error: ${this.error}`;
+      setTimeout(() => {
+        this.error = "";
+        el.innerHTML = "";
+      }, 1000)
+    }});
+  }
+
+  private err_leave(el: any, done: any){
+    gsap.to(el, {height: 0, padding: 0, opacity: 0, onComplete:done});
+  }
+
   private getData(type: string){
     let query = '';
     if(type == "latlon"){
@@ -58,7 +80,7 @@ export default class App extends Vue {
     } else if(type == "name") {
       query = `q=${this.location}`;
     }
-    
+
     let url = 'https://api.openweathermap.org/data/2.5/weather?' + query + '&APPID=' + this.appID + '&units=imperial';
 
     axios.get(url)
@@ -76,18 +98,64 @@ export default class App extends Vue {
 html, body{
   margin: 0px;
   padding: 0px;
+  touch-action: none;
+  height:100%;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
   }
+  #widget {
+    position: relative;
+    background-color:#CCC;
+    width: 500px;
+    height: 250px;
+    border-radius: 15px 15px 0px 0px;
+    box-sizing: border-box;
+    }
+
 .error{
   width: 100%;
+  height: 40px;
   padding: 10px;
+  position: absolute;
   background-color:#F00;
+  border-radius: 15px 15px 0px 0px;
   text-align: center;
   margin: 0px 0px 10px 0px;
-}
+  box-sizing: border-box;
+  z-index: 9999;
+  }
+
+  #curLoc{
+    padding: 10px;
+    display: inline-block;
+    text-align: center;
+    background-color: #AAA;
+    text-decoration: none;
+    width: 100%;
+    box-sizing: border-box;
+    color:#333;
+    font-weight: bold;
+    }
+  #locinput{
+    box-sizing: border-box;
+    position: absolute;
+    bottom: -90px;
+    width: 100%;
+    }
+    #locinput input{
+      outline: none;
+      border-radius: 10px;
+      border: 3px solid;
+      box-sizing: border-box;
+      width: 100%;
+      padding: 10px;
+      }
 
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="map">
-    <canvas ref="canvas" style="border: 1px solid;" />
+    <canvas ref="canvas" />
+    <div id="latlon" style="position: absolute;">
+      <span><strong>Lat</strong> {{lat}}</span>
+      <span><strong>Lon</strong> {{lon}}</span>
+    </div>
+    <div style="position: relative; border-radius: 15px; width: 100%; height: 100%; overflow: hidden;">
+      <img src="../assets/world.svg" style="position: absolute;top: -95px;" width="500" alt="">
+    </div>
   </div>
 </template>
 
@@ -8,59 +15,100 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class WeatherWidget extends Vue {
+export default class Map extends Vue {
   @Prop() private data!: object;
+  private can!: HTMLCanvasElement;
+  private ctx!: CanvasRenderingContext2D;
+  private x!: number;
+  private y!: number;
+
+  private lat: number = 0;
+  private lon: number = 0;
 
   private mounted(){
-    let can = this.$refs.canvas as HTMLCanvasElement;
-    let ctx = can.getContext('2d') as CanvasRenderingContext2D;
 
-    let md = false;
+    this.can = this.$refs.canvas as HTMLCanvasElement;
+    this.can.width = this.$el.clientWidth - 25;
+    this.can.height = this.$el.clientHeight - 20;
+    this.ctx = this.can.getContext('2d') as CanvasRenderingContext2D;
 
-    can.width = 500;
-    can.height = 500;
+    let img = new Image();
+    img.src = "../assets/world.svg";
+    img.onload = () => {
+      this.ctx.drawImage(img, 0, 0);
+    }
 
-    can.addEventListener('pointerdown', (e) => {
-      md = true;
-      can.setPointerCapture(e.pointerId);
+
+    this.x = this.can.width/2;
+    this.y = this.can.height/2;
+
+    this.draw();
+
+    this.can.addEventListener('pointerdown', (e) => {
+      let _x = e.offsetX;
+      let _y = e.offsetY;
+      this.x = _x - this.can.width * Math.floor(_x / this.can.width);
+      this.y = _y - this.can.height * Math.floor(_y / this.can.height);
+
+      this.draw();
     });
-    can.addEventListener('pointerup', (e) => {
-      md = false;
-    });
-    can.addEventListener('pointermove', (e) => {
-      let pos = {
-        x: e.offsetX,
-        y: e.offsetY
-      }
+
+    this.can.addEventListener('pointermove', (e) => {
+      e.preventDefault();
 
       // Capture mouse wrap around
-      if(pos.x >= can.width){
-        pos.x -= can.width * Math.floor(pos.x/can.width) + 1;
-      } else if(pos.x <= 0){
-        pos.x += can.width * Math.floor(pos.x/can.width) + 1;
+      let _x = e.offsetX;
+      let _y = e.offsetY;
+
+      this.x = _x - this.can.width * Math.floor(_x / this.can.width);
+      this.y = _y - this.can.height * Math.floor(_y / this.can.height);
+
+      if(e.pressure){
+        this.can.setPointerCapture(e.pointerId);
+        this.lat = this.x;
+        this.lon = this.y;
+
+        this.draw();
       }
-
-      if(pos.y >= can.height){
-        pos.y -= can.height * Math.floor(pos.y/can.height) + 1;
-
-      }
-      e.preventDefault();
-      ctx.fillStyle = "#F00";
-
-      if(md){
-        ctx.clearRect(0,0,can.width, can.height);
-        ctx.fillRect(pos.x, 0, 1, can.height);
-        ctx.fillRect(0, pos.y, can.width, 1);
-      }
-
     })
+  }
+
+  private draw(){
+    this.ctx.fillStyle = "#F00";
+    this.ctx.clearRect(0,0,this.can.width, this.can.height);
+    this.ctx.fillRect(this.x, 0, 1, this.can.height);
+    this.ctx.fillRect(0, this.y, this.can.width, 1);
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#map{
+  padding: 10px;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  }
+
 canvas{
-  border: 1px solid;
-}
+  border: 2px solid #AAA;
+  border-radius: 15px 15px 0px 0px;
+  position: absolute;
+  z-index: 9998;
+  }
+  #latlon{
+    position: absolute;
+    z-index: 9998;
+    bottom: 10px;
+    right: 15px;
+    }
+    #latlon span{
+      padding: 5px;
+      margin: 4px;
+      display: inline-block;
+      width: 80px;
+      background-color: rgba(0,0,0,.4);
+      }
 </style>
